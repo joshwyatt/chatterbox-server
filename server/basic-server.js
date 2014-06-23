e08c55358021a31c41c13ec00106f761dd0251d7
Hack Reactor Students <accounts@hackreactor.com>
2014-06-23 15:19:54 -0700

    hook server upto browser

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,6 +1,7 @@
 /* Import node's http module: */
 var http = require("http");
 
+var handleRequest = require("./request-handler.js").handleRequest;
 
 /* Every server needs to listen on a port with a unique number. The
  * standard port for HTTP servers is port 80, but that port is
@@ -12,7 +13,6 @@ var port = 3000;
  * we'll have it listen on the IP address 127.0.0.1, which is a
  * special address that always refers to localhost. */
 var ip = "127.0.0.1";
-
 
 
 /* We use node's http module to create a server. Note, we called it 'server', but
--- server/request-handler.js
+++ server/request-handler.js
@@ -5,7 +5,7 @@
  * this file and include it in basic-server.js so that it actually works.
  * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
 
-var handleRequest = function(request, response) {
+exports.handleRequest = function(request, response) {
   /* the 'request' argument comes from nodes http module. It includes info about the
   request - such as what URL the browser is requesting. */
 
@@ -38,7 +38,7 @@ var handleRequest = function(request, response) {
  * like file://your/chat/client/index.html, which is considered a
  * different domain.) */
 var defaultCorsHeaders = {
-  "access-control-allow-origin": "*",
+  "access-control-allow-origin": "./Users/HR10/Code/foresttoney/2014-06-chatterbox-server/client/index.html",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
   "access-control-max-age": 10 // Seconds.
